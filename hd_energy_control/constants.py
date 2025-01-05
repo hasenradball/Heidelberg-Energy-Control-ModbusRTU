e0126abeba127e371d543c6568ace2f58dd59278
Hasenradball <mail@frankhaefele.de>
2025-01-05 14:42:45 +0100

    review of latest release, prepare fixes

--- hd_energy_control/constants.py
+++ hd_energy_control/constants.py
@@ -1,9 +1,9 @@
-""""Constants Module for the HD Energy Control"""
+"""Module providing constants for HD Energy Control"""
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class HdEcConstants:
-    """Constants for use with pymodbus
+class HDEnergyControlConstants:
+    """constants for use with pymodbus
     """
     TYPE_TO_LENGTH = {'U8': 1, 'U16': 1, 'U32': 2, 'U64': 4, \
                       'S8': 1, 'S16': 1, 'S32': 2, 'S64': 4}
