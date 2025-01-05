6ec9d06f2a488e53ad6dc736dc9b1f07c4dc0d42
Hasenradball <mail@frankhaefele.de>
2025-01-05 16:44:13 +0100

    add example for mariadb_config.py

---
+++ examples/mariadb_config.py
@@ -0,0 +1,9 @@
+""""Config for mariaDB"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+MARIA_DB_CONFIG = {'user': username, \
+          'password': password, \
+          'database': db_name, \
+          'host': host_or_ip, \
+          'port': dp_port}
